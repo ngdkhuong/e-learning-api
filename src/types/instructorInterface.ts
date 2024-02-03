a0ee05fa63c03b63a9d4f1113afc6d6250243477
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-03 22:32:26 +0700

    instructor interface

---
+++ src/types/instructorInterface.ts
@@ -0,0 +1,32 @@
+export interface InstructorInterface {
+    firstName: string;
+    lastName: string;
+    profilePic: {
+        name: string;
+        key?: string;
+        url?: string;
+    };
+    email: string;
+    mobile: string;
+    qualifications: string;
+    subjects: string;
+    experience: string;
+    skills: string;
+    about: string;
+    password: string;
+    certificates: Certificate[];
+}
+
+export interface Certificate {
+    name: string;
+    url?: string;
+    key?: string;
+}
+
+export interface SavedInstructorInterface extends InstructorInterface {
+    _id: string;
+    isVerified: boolean;
+    dateJoined: Date;
+    coursesCreated: Array<string>;
+    profileUrl: string;
+}
