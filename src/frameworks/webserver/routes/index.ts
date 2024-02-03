ed7e0d6de4025a3b93dd5f9f82cb5a86c472999d
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-03 12:01:26 +0700

    routes auth

--- src/frameworks/webserver/routes/index.ts
+++ src/frameworks/webserver/routes/index.ts
@@ -1,10 +1,12 @@
 import { Application } from 'express';
 import { RedisClient } from '../../../app';
+import authRouter from './auth.ts';
 
 const routes = (app: Application, redisClient: RedisClient) => {
     app.use('/api/test', () => {
         console.log('Server Testing');
     });
+    app.use('/api/auth', authRouter());
 };
 
 export default routes;
