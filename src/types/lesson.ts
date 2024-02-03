b48f3f76e5c80c8fe7f62bfdd39b9a5a6f202fe7
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-03 22:32:15 +0700

    lesson interface

---
+++ src/types/lesson.ts
@@ -0,0 +1,37 @@
+export interface CreateLessonInterface {
+    title: string;
+    description: string;
+    contents: string[];
+    duration: number;
+    instructorId: string;
+    courseId: string;
+    media: {
+        name: string;
+        key: string;
+    }[];
+    questions: Question[];
+}
+
+export interface EditLessonInterface {
+    title?: string;
+    description?: string;
+    contents?: string[];
+    duration?: number;
+    instructorId?: string;
+    courseId?: string;
+    media?: {
+        name: string;
+        key: string;
+    }[];
+    questions?: Question[];
+}
+
+export interface Question {
+    question: string;
+    options: Option[];
+}
+
+export interface Option {
+    option: string;
+    isCorrect: boolean;
+}
