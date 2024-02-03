10ede30f16504ef03fb8a60b19093e738f0be902
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-03 12:02:16 +0700

    student repository

---
+++ src/frameworks/database/mongodb/repositories/studentsRepoMongoDb.ts
@@ -0,0 +1,16 @@
+import { StudentInterface } from '../../../../types/studentInterface';
+import Student from '../models/student';
+import mongoose from 'mongoose';
+import { StudentRegisterInterface } from '@src/types/studentRegisterInterface';
+
+export const studentRepositoryMongoDB = () => {
+    const addStudent = async (student: StudentRegisterInterface) => {
+        const newStudent = new Student(student);
+        return await newStudent.save();
+    };
+
+    const getStudentByEmail = async (email: string) => {
+        const user: StudentInterface | null = await Student.findOne({ email });
+        return user;
+    };
+};
