c82a845b4df1970d7fa491ed08c456c860b37001
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-03 22:31:09 +0700

    course interface

---
+++ src/types/courseInterface.ts
@@ -0,0 +1,69 @@
+import { Question } from './lesson';
+
+interface fileSchema {
+    key: string;
+    name: string;
+    url?: string;
+}
+
+export interface AddCourseInfoInterface {
+    title: string;
+    duration: number;
+    category: string;
+    level: string;
+    tags: string[] | string;
+    price: number;
+    isPaid: boolean;
+    about: string;
+    description: string;
+    syllabus: string[] | string;
+    requirements: string[] | string;
+    thumbnail: fileSchema;
+    introduction: fileSchema;
+    guidelines: fileSchema;
+    instructorId: string;
+    rating: number;
+    isVerified: boolean;
+}
+
+export interface CourseInterface extends AddCourseInfoInterface {
+    coursesEnrolled: Array<string>;
+    thumbnailUrl: string;
+    introductionUrl: string;
+    guidelinesUrl: string;
+}
+
+export interface AddQuizInfoInterface {
+    courseId: string;
+    lessonId: string;
+    questions: Question[];
+}
+
+export interface EditQuizInfoInterface {
+    courseId: string;
+    lessonId: string;
+    questions: Question[];
+}
+
+export interface EditCourseInfo {
+    title?: string;
+    duration?: number;
+    category?: string;
+    tags?: string[] | string;
+    price?: number;
+    isPaid?: boolean;
+    about?: string;
+    description?: string;
+    syllabus?: string[] | string;
+    requirements?: string[] | string;
+    thumbnail?: fileSchema;
+    introductionVideo?: fileSchema;
+    guidelines?: fileSchema;
+    instructorId?: string;
+    rating?: number;
+    isVerified?: boolean;
+    enrollmentCount?: number;
+    quiz?: AddQuizInfoInterface;
+    enrollmentLimit?: number;
+    completionStatus?: number;
+}
