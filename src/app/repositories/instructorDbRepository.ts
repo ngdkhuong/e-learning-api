4265ab08f2d37a8250ca45b04e0af3b4e9994b73
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-06 11:57:12 +0700

    instructor auth

---
+++ src/app/repositories/instructorDbRepository.ts
@@ -0,0 +1,3 @@
+import { InstructorInterface } from '@src/types/instructorInterface';
+import { InstructorRepositoryMongoDb } from '@src/frameworks/database/mongodb/repositories/instructorRepoMongoDb';
+import { SavedInstructorInterface } from '@src/types/instructorInterface';
---
+++ src/app/usecases/auth/instructorAuth.ts
@@ -0,0 +1,12 @@
+import { InstructorInterface, SavedInstructorInterface } from '../../../types/instructorInterface';
+import { InstructorDbInterface } from '../../../app/repositories/instructorDbRepository';
+import { AuthServiceInterface } from '../../../app/services/authServiceInterface';
+import { RefreshTokenDbInterface } from '@src/app/repositories/refreshTokenDBRepository';
+import { UploadFileInterface } from '@src/types/common';
+import { CloudServiceInterface } from '@src/app/services/cloudServiceInterface';
+
+export const instructorRegister = async (
+    instructor: InstructorInterface,
+    files: Express.Multer.File[],
+    instructorRepository: ReturnType<InstructorDbInterface>,
+) => {};
