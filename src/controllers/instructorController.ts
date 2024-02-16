a3d61ede42cb300d7ac2e2010b68896b3c62d491
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-16 22:42:14 +0700

    instructor ctrl

--- src/controllers/instructorController.ts
+++ src/controllers/instructorController.ts
@@ -1,9 +1,13 @@
+import { CourseDbRepositoryInterface } from '@src/app/repositories/courseDbRepository';
 import { InstructorDbInterface } from '@src/app/repositories/instructorDbRepository';
 import { AuthServiceInterface } from '@src/app/services/authServiceInterface';
 import { CloudServiceInterface } from '@src/app/services/cloudServiceInterface';
+import { SendEmailServiceInterface } from '@src/app/services/sendEmailServiceInterface';
+import { CourseRepositoryMongoDbInterface } from '@src/frameworks/database/mongodb/repositories/courseRepoMongoDb';
 import { InstructorRepositoryMongoDb } from '@src/frameworks/database/mongodb/repositories/instructorRepoMongoDb';
 import { AuthService } from '@src/frameworks/services/authService';
 import { CloudServiceImpl } from '@src/frameworks/services/s3CloudService';
+import { SendEmailService } from '@src/frameworks/services/sendEmailService';
 
 const instructorController = (
     authServiceInterface: AuthServiceInterface,
