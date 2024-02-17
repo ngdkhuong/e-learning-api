1c3c3e8010a1392e5134fc6a8258d2029fda3b56
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-17 23:03:43 +0700

    add route instructor & refresh token

--- src/frameworks/webserver/routes/index.ts
+++ src/frameworks/webserver/routes/index.ts
@@ -2,12 +2,19 @@ import { Application } from 'express';
 import { RedisClient } from '../../../app';
 import authRouter from './auth';
 import studentRouter from './student';
+import refreshRouter from './refresh';
+import instructorRouter from './instructor';
 
 const routes = (app: Application, redisClient: RedisClient) => {
+    // * Testing Router
     app.use('/api/test', () => {
         console.log('Server Testing');
     });
+
+    // * Router
     app.use('/api/auth', authRouter());
+    app.use('/api/all/refresh-token', refreshRouter());
+    app.use('/api/instructors', instructorRouter());
     app.use('/api/students', studentRouter(redisClient));
 };
 
