83dd8c6fe8eb9030a4ceebb55f177bf213d92a15
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-20 16:34:16 +0700

    add payment repo

---
+++ src/app/repositories/paymentDbRepository.ts
@@ -0,0 +1,21 @@
+import { PaymentInfo } from '../../types/payment';
+import { PaymentImplInterface } from '../../frameworks/database/mongodb/repositories/paymentRepoMongoDb';
+
+export const paymentInterface = (repository: ReturnType<PaymentImplInterface>) => {
+    const savePayment = async (paymentInfo: PaymentInfo) => await repository.savePaymentInfo(paymentInfo);
+
+    const getMonthlyRevenue = async () => await repository.getMonthlyRevenue();
+
+    const getRevenueForEachMonth = async () => await repository.getRevenueForEachMonth();
+
+    const getCoursesEnrolledPerMonth = async () => await repository.getCoursesEnrolledPerMonth();
+
+    return {
+        savePayment,
+        getMonthlyRevenue,
+        getRevenueForEachMonth,
+        getCoursesEnrolledPerMonth,
+    };
+};
+
+export type PaymentInterface = typeof paymentInterface;
