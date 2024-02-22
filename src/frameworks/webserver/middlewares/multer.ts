59563bbc2a0608c2509ed734929356dfd83931f1
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-22 12:31:17 +0700

    multer update

--- src/frameworks/webserver/middlewares/multer.ts
+++ src/frameworks/webserver/middlewares/multer.ts
@@ -1,6 +1,10 @@
 import multer from 'multer';
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
 
 const upload = multer({
     storage: storage,
