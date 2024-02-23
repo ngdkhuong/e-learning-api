23b6fc2b74426676a0dd48fac9b40c74e03363c0
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-23 15:32:39 +0700

    api created to edit category

---
+++ src/app/repositories/categoryDbRepository.ts
@@ -0,0 +1,25 @@
+import { CategoryRepoMongoDbInterface } from '../../frameworks/database/mongodb/repositories/categoryRepoMongoDb';
+import { CategoryInterface } from './../../types/category';
+
+export const categoryDbInterface = (repository: ReturnType<CategoryRepoMongoDbInterface>) => {
+    const addCategory = async (category: CategoryInterface) => await repository.addCategory(category);
+
+    const getCategoryById = async (categoryId: string) => await repository.getCategoryById(categoryId);
+
+    const getAllCategory = async () => await repository.getAllCategory();
+
+    const editCategory = async (categoryId: string, categoryInfo: { name: string; description: string }) =>
+        await repository.editCategory(categoryId, categoryInfo);
+
+    const getCourseCountByCategory = async () => await repository.getCourseCountByCategory();
+
+    return {
+        addCategory,
+        getCategoryById,
+        getAllCategory,
+        editCategory,
+        getCourseCountByCategory,
+    };
+};
+
+export type CategoryDbInterface = typeof categoryDbInterface;
